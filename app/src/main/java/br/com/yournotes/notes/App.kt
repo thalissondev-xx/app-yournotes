9da698f07b7198396a516cbfc8364a3fe9c2b261
thalissondev <thalissondev@gmail.com>
2017-04-13 00:49:33 -0300

    - Convert the Application class for Kotlin

---
+++ app/src/main/java/br/com/yournotes/notes/App.kt
@@ -0,0 +1,12 @@
+package br.com.yournotes.notes
+
+import android.app.Application
+
+/**
+ * Created by thalissonestrela on 4/13/17.
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
